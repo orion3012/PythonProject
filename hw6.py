cef4d9ccaa237fad770e5869fd5f15c2c3987f0d
orion3012 <browny30032011@gmail.com>
2025-03-27 14:54:36 +0100

    дз 6

---
+++ hw6.py
@@ -0,0 +1,22 @@
+result = []
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a/b
+
+data = {10: 2, 2: 5, 123: 4, 18: 0, 0: 15, 8 :4}
+
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except ValueError:
+        print("value error")
+    except IndexError:
+        print("index error")
+    except ZeroDivisionError:
+        print("zero division error")
+
+print(result)
